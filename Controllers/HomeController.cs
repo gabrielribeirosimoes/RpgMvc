7e88918cae3073f2cb7294d0071316877b658e88
Gabriel Ribeiro Simões <gabrielribeirosimoes@hotmail.com>
2023-12-12 22:00:59 -0300

    subindo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RpgMvc.Models;
+
+namespace RpgMvc.Controllers;
+
+public class HomeController : Controller //: herança
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index() //isso é um get
+    {
+        return View();// se o view estiver vazio ele vai chamar a pagina citada acima, nesse caso ele chamará a pg Index 
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,189 @@
+using Microsoft.AspNetCore.Mvc;
+using RpgMvc.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+
+namespace RpgMvc.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public string uriBase = "http://lzsouza.somee.com/RpgApi/Usuarios/"; //endereço do somee do professor
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("CadastrarUsuario");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> RegistrarAsync(UsuarioViewModel u)
+        {
+            try
+            {
+                //proximo codigo aqui
+                HttpClient httpClient = new HttpClient();
+                string uriCompletar = "Registrar";
+
+                var content = new StringContent(JsonConvert.SerializeObject(u));
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriCompletar, content); // uri base é o local do somee e o completar é o complemento
+
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    TempData["Mensagem"] =
+                        string.Format("Usuário {0} Registrado com sucesso! Faça o login para acessar.", u.Username);
+                    return View("AutenticarUsuario");
+                }
+
+                else
+                {
+                    throw new System.Exception(serialized);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message; //antes do = é uma variavel temporaria do c#
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+
+        public ActionResult IndexLogin()
+        {
+            return View("AutenticarUsuario");
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult> AutenticarAsync(UsuarioViewModel u)
+        {
+            HttpClient httpClient = new HttpClient();
+            string uriCompletar = "Autenticar";
+
+            var content = new StringContent(JsonConvert.SerializeObject(u));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriCompletar, content);
+
+            string serialized = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                UsuarioViewModel uLogado = JsonConvert.DeserializeObject<UsuarioViewModel>(serialized);
+                HttpContext.Session.SetString("SessionTokenUsuario", uLogado.Token);
+
+                HttpContext.Session.SetString("SessionUserName", uLogado.Username);
+                TempData["Mensagem"] = string.Format("Bem-Vindo{0}!!!", uLogado.Username);
+                return RedirectToAction("Index", "Personagens");
+            }
+            else
+            {
+                throw new System.Exception(serialized);
+            }
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> IndexInformacoesAsync()
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                //Novo: Recuperação informação da sessão
+
+                string login = HttpContext.Session.GetString("SessionUsername");
+
+                string uriComplementar =
+                $"GetByLogin/{login}";
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase +
+                uriComplementar);
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    UsuarioViewModel u = await Task.Run(() =>
+                    JsonConvert.DeserializeObject<UsuarioViewModel>(serialized));
+                    return View(u);
+                }
+                else
+                {
+                    throw new System.Exception(serialized);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AlterarEmail(UsuarioViewModel u)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                string uriComplementar = "AtualizarEmail";
+                var content = new StringContent(JsonConvert.SerializeObject(u));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                HttpResponseMessage response = await httpClient.PutAsync(uriBase + uriComplementar, content);
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    TempData["Mensagem"] = "E-mail alterado com sucesso.";
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+            }
+            return RedirectToAction("IndexInformacoes");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AlterarSenha(UsuarioViewModel u)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new
+                AuthenticationHeaderValue("Bearer", token);
+                string uriComplementar = "AlterarSenha";
+                u.Username = HttpContext.Session.GetString("SessionUsername");
+                var content = new StringContent(JsonConvert.SerializeObject(u));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PutAsync(uriBase +
+                uriComplementar, content);
+                string serialized = await response.Content.ReadAsStringAsync();
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    string mensagem = "Senha alterada com sucesso.";
+                    TempData["Mensagem"] = mensagem; //Mensagem guardada do TempData que aparcerá na página pai do modal
+                return Json(mensagem); //Mensagem que será exibida no alert da Função que chamou este método
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                return Json(ex.Message);
+            }
+        }
+
+
+
+
+
+
+    }
+}
